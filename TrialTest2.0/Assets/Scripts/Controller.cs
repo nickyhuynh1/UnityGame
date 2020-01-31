8d146493887e83119a913f7d6c657b3f48d24cec
nhuynh18 <nhuynh18@vt.edu>
2020-01-31 17:19:41 -0500

    my test game

---
+++ TrialTest2.0/Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Transform rtouch;
+    public Transform ltouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rtouch.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        ltouch.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+    }
+}
---
+++ TrialTest2.0/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+ 
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,2000 * Time.deltaTime);
+    }
+}
